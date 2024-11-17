f9f452e7af16755a35aeaa5e4d168a4c0802fb18
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-11-17 20:38:08 +0700

    Create black scholes model.py

---
+++ black scholes model.py
@@ -0,0 +1,11 @@
+class BlackScholesModel:
+    def __init__(self, S, K, T, r, sigma):
+        self.S = S        # Underlying asset price
+        self.K = K        # Option strike price
+        self.T = T        # Time to expiration in years
+        self.r = r        # Risk-free interest rate
+        self.sigma = sigma  # Volatility of the underlying asset
+
+
+
+
